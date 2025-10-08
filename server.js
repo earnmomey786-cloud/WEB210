444de83d6d7ab482518c7002cde5973e34b39ef9
Tabeer Ashi <SDelmary@my.gcu.edu>
2025-10-08 23:18:54 +0200

    🔧 Fix: Usar middleware en vez de ruta para Express 5 compatibility

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(join(__dirname, 'dist')));
 
 // Todas las rutas devuelven index.html (para React Router)
-app.get('/*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
